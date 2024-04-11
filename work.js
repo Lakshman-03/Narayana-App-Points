d69c6b25081ba426268ba204e99f7c3c4e671c0f
Lakshman-03 <kayalalakshman01@gmail.com>
2024-04-11 06:39:54 +0530

    v 1.0 version

--- work.js
+++ work.js
@@ -6,7 +6,7 @@ import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
 const app = express();
-const port = 10000;
+const port = 3000;
 env.config();
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended : true}))
@@ -20,13 +20,20 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
   
-const db =new  pg.Client({
-    user: process.env.USER,
-    database : process.env.DATABASE,
-    port: process.env.PORT,
-    host:process.env.HOST,
-    password: process.env.PASSWORD
-})
+// const db =new  pg.Client({
+//     user: process.env.USER,
+//     database : process.env.DATABASE,
+//     port: process.env.PORT,
+//     host:process.env.HOST,
+//     password: process.env.PASSWORD
+// })
+const db = new pg.Client({
+    connectionString: process.env.DATABASE_URL, 
+    ssl: {
+      rejectUnauthorized: false 
+    }
+});
+
 
 db.connect();
 app.get("/",(req,res)=>{
@@ -38,10 +45,15 @@ app.get("/Authentication",(req,res)=>{
     }else{
       res.redirect("/")
     }
-  })
+  }
+)
 
-app.get("/profile",(req,res)=>{
+app.get("/profile",async(req,res)=>{
     const userData = req.user;
+    const result= await db.query("select * from app where student_id = $1",[userData.id])
+    let prog = result.rows[0]
+    let percent  = (prog.totalapp/160)*100
+    console.log(percent)
     res.render("profile.ejs",{
         name: userData.name,
         rollNo: userData.id,
@@ -51,15 +63,38 @@ app.get("/profile",(req,res)=>{
         branch: userData.branch,
         section: userData.section,
         sem: "II",
-        image: userData.profile_pic
+        image: userData.profilepic,
+        progess:Math.floor(percent),
+        total : prog.totalapp
     });
 });
 
+// app.get("/dash",(req,res)=>{
+// res.render("dashboard.ejs")
+// })
 
+app.get("/details",(req,res)=>{
+    res.render("details.ejs");
+})
+
+app.get("/request",(req,res)=>{
+    res.render("request.ejs")
+})
 app.get("/wait",(req,res)=>{
     res.render("login.ejs")
 })
 
+app.get("/about",(req,res)=>{
+    res.render("about.ejs")
+})
+
+app.get("/nbot",(req,res) =>{
+
+    res.render("nbot.ejs")
+})
+app.get("/logout",(req,res)=>{
+    res.redirect("/")
+})
 
 // app.post("/student-login",passport.authenticate("local",{
 //     successRedirect:"/Authentication",
